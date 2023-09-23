f07b6c899a9684289af2c45851d5c724436418ed
Abrar Jahin <abrar.j.official@gmail.com>
2023-09-24 03:49:54 +0600

    September 24, 2023

--- Operating System/Segmentation.py
+++ Operating System/Segmentation.py
@@ -8,14 +8,13 @@ for i in range(seg):
     a.append(int(input("     Enter Limit : ")))
     seg_table.append(a)
     print()
-for i in range(seg):
-    print(f"{seg_table[i][0]}, {seg_table[i][1]}")
 print("Enter The Logical Address : ")
 s = int(input("     Enter Segment No. : "))
-b = int(input("     Enter Byte No. : "))
-if 0 < b <= seg_table[s][1]:
-    print(f"The Physical Address Is : {seg_table[s][0]+b}")
-elif s > seg:
-    print("Invalid Segment.")
+if s <= seg:
+    b = int(input("     Enter Byte No. : "))
+    if 0 < b <= seg_table[s][1]:
+        print(f"The Physical Address Is : {seg_table[s][0]+b}")
+    else:
+        print("It's a Trap.")
 else:
-    print("It's a Trap.")
+    print("Invalid Segment.")
